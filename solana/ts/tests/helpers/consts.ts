85c09a84e993404d177186db6cf31e1bb1dbfe34
Ben Guidarelli <ben.guidarelli@gmail.com>
2024-05-01 14:30:54 -0400

    set core bridge pid to mainnet

--- solana/ts/tests/helpers/consts.ts
+++ solana/ts/tests/helpers/consts.ts
@@ -1,6 +1,6 @@
 import { Keypair, PublicKey } from "@solana/web3.js";
 
-export const CORE_BRIDGE_PID = new PublicKey("3u8hJUVTA4jH1wYAyUur7FFZVQ8H635K3tSHHF4ssjQ5");
+export const CORE_BRIDGE_PID = new PublicKey("worm2ZoG2kUd4vFXhvjh93UUH596ayRfgQ2MgjNMTth");
 export const GUARDIAN_KEY = "cfb12303a19cde580bb4dd771639b0d26bc68353645571a8cff516ab2ee113a0";
 
 export const FEE_UPDATER_KEYPAIR = Keypair.fromSecretKey(
