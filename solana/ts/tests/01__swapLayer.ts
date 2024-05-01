2dfe14334608112e84cf2542a282ec8f95554bf3
Ben Guidarelli <ben.guidarelli@gmail.com>
2024-05-01 14:10:14 -0400

    minimal changes to get ts to build

--- solana/ts/tests/01__swapLayer.ts
+++ solana/ts/tests/01__swapLayer.ts
@@ -505,7 +505,7 @@ async function createAndRedeemCctpFillForTest(
     orderSender: number[],
     wormholeSequence: bigint,
     redeemerMessage: Buffer,
-): Promise<void | { vaa: PublicKey; message: LiquidityLayerMessage }> {
+): Promise<null | { vaa: PublicKey; message: LiquidityLayerMessage }> {
     const encodedMintRecipient = Array.from(tokenRouter.cctpMintRecipientAddress().toBuffer());
     const sourceCctpDomain = 0;
     const amount = 6900000000n;
@@ -536,6 +536,7 @@ async function createAndRedeemCctpFillForTest(
             },
             {
                 fill: {
+                    // @ts-ignore
                     sourceChain: foreignChain as wormholeSdk.ChainId,
                     orderSender,
                     redeemer: Array.from(redeemer.toBuffer()),
--- solana/ts/tests/helpers/consts.ts
+++ solana/ts/tests/helpers/consts.ts
@@ -1,4 +1,7 @@
-import { Keypair } from "@solana/web3.js";
+import { Keypair, PublicKey } from "@solana/web3.js";
+
+export const CORE_BRIDGE_PID = new PublicKey("3u8hJUVTA4jH1wYAyUur7FFZVQ8H635K3tSHHF4ssjQ5");
+export const GUARDIAN_KEY = "cfb12303a19cde580bb4dd771639b0d26bc68353645571a8cff516ab2ee113a0";
 
 export const FEE_UPDATER_KEYPAIR = Keypair.fromSecretKey(
     Buffer.from(
