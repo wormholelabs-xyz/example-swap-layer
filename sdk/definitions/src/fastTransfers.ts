c4e3530d8b139e1ba9a440805882e70208777e5f
Kevin Peters <kevin@wormholelabs.xyz>
2024-05-16 14:41:37 -0500

    first commit

---
+++ sdk/definitions/src/fastTransfers.ts
@@ -0,0 +1,8 @@
+import {
+  type Chain,
+  type Network,
+} from "@wormhole-foundation/sdk-base";
+
+export interface FastTransfers<N extends Network, C extends Chain> {
+    // TODO: implement
+}
---
+++ sdk/definitions/src/index.ts
@@ -0,0 +1,10 @@
+// import { registerPayloadTypes } from "@wormhole-foundation/sdk";
+// import { nttNamedPayloads } from "./layouts/index.js";
+
+// registerPayloadTypes("FastTransfers", nttNamedPayloads);
+
+export * from "./fastTransfers.js";
+
+// export * from "./layouts/index.js";
+// export type * from "./layouts/index.js";
+
