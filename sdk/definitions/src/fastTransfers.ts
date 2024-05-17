666959c88712811e0a174bdfee338d24b7e605d5
Kevin Peters <kevin@wormholelabs.xyz>
2024-05-17 16:38:40 -0500

    progress on interface

--- sdk/definitions/src/fastTransfers.ts
+++ sdk/definitions/src/fastTransfers.ts
@@ -1,8 +1,41 @@
+import { type Chain, type Network } from "@wormhole-foundation/sdk-base";
+
 import {
-  type Chain,
-  type Network,
-} from "@wormhole-foundation/sdk-base";
+  AccountAddress,
+  ChainAddress,
+  EmptyPlatformMap,
+  UnsignedTransaction,
+} from "@wormhole-foundation/sdk-definitions";
+
+import { OutputToken, RedeemMode } from "./layouts/index.js";
+
+export namespace FastTransfers {
+  const _protocol = "FastTransfers";
+  export type ProtocolName = typeof _protocol;
+
+  //export type InputToken<C extends Chain> = {
+  //  type: "Usdc" | "Gas" | "Other";
+  //  amount: bigint;
+  //};
+}
 
 export interface FastTransfers<N extends Network, C extends Chain> {
-    // TODO: implement
+  transfer(
+    sender: AccountAddress<C>,
+    recipient: ChainAddress, // TODO: `destination` is better name?
+    inputToken: any, // TODO: what type shoudl this be? Should be dependent on the chain
+    outputToken: OutputToken,
+    redeemMode: RedeemMode
+  ): AsyncGenerator<UnsignedTransaction<N, C>>;
+}
+
+declare module "@wormhole-foundation/sdk-definitions" {
+  export namespace WormholeRegistry {
+    interface ProtocolToInterfaceMapping<N, C> {
+      FastTransfers: FastTransfers<N, C>;
+    }
+    interface ProtocolToPlatformMapping {
+      FastTransfers: EmptyPlatformMap<FastTransfers.ProtocolName>;
+    }
+  }
 }
