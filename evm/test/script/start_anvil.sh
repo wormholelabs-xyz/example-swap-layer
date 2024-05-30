bf7dd63fb887f7ea2f0281fd7358f35574648c47
A5 Pickle <a5-pickle@users.noreply.github.com>
2024-05-30 17:23:40 -0500

    e2e: rename directory; fix run_test.sh to use anchor

--- evm/test/script/start_anvil.sh
+++ evm/test/script/start_anvil.sh
@@ -9,24 +9,37 @@ fi
 ROOT=$(dirname $0)
 ENV=$ROOT/../../env/localnet
 
+OWNER_PRIVATE_KEY=0x646f1ce2fdad0e6deeeb5c7e8e5543bdde65e86029e2fd9fc169899c440a7913
+
 . $ENV/Ethereum.env
 
 # Ethereum (CCTP).
 anvil --port 8548 \
     -m "myth like bonus scare over problem client lizard pioneer submit female collect" \
-    --no-mining \
-    --fork-url $RELEASE_RPC > /dev/null &
+    --fork-url $RELEASE_RPC \
+    > /dev/null &
+
+sleep 2
+
+forge script $ROOT/DeploySwapLayerForTest.s.sol \
+    --rpc-url http://localhost:8548 \
+    --broadcast \
+    --private-key $OWNER_PRIVATE_KEY
 
 . $ENV/Base.env
 
 # Base (CCTP).
 anvil --port 8549 \
     -m "myth like bonus scare over problem client lizard pioneer submit female collect" \
-    --no-mining \
-    --fork-url $RELEASE_RPC > /dev/null &
-
+    --fork-url $RELEASE_RPC \
+    > /dev/null &
 
 sleep 2
+
+forge script $ROOT/DeploySwapLayerForTest.s.sol \
+    --rpc-url http://localhost:8549 \
+    --broadcast \
+    --private-key $OWNER_PRIVATE_KEY
 
 # Double-check number of anvil instances.
 if [ "$( pgrep anvil | wc -l )" -ne 2 ]; then
