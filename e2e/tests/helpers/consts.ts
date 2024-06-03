8d5b699d06a22153a7913836117d0789b421460b
gator-boi <gator-boi@users.noreply.github.com>
2024-06-03 12:02:18 -0500

    e2e: handle evm setup

--- e2e/tests/helpers/consts.ts
+++ e2e/tests/helpers/consts.ts
@@ -6,6 +6,8 @@ export const SOLANA_SWAP_LAYER_ID = "SwapLayer1111111111111111111111111111111111
 export const USDC_MINT_ADDRESS = new PublicKey("EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v");
 
 export const EVM_PRIVATE_KEY = "0x395df67f0c2d2d9fe1ad08d1bc8b6627011959b79c53d7dd6a3536a33ab8a4fd";
+export const GUARDIAN_PRIVATE_KEY =
+    "cfb12303a19cde580bb4dd771639b0d26bc68353645571a8cff516ab2ee113a0";
 
 // Avalanche Mainnet Fork
 export const EVM_LOCALHOSTS = {
--- e2e/tests/helpers/index.ts
+++ e2e/tests/helpers/index.ts
@@ -1,2 +1,3 @@
 export * from "./env";
 export * from "./consts";
+export * from "./fork";
